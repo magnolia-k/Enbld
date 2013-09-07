1639a91f3f1d01b8a57f7448b10bb40a5e99c08f
magnolia <magnolia.k@me.com>
2013-09-07 14:34:14 +0900

    0.6011

--- lib/Blender/Declare.pm
+++ lib/Blender/Declare.pm
@@ -6,7 +6,7 @@ use warnings;
 use FindBin qw/$Script/;
 use Getopt::Long;
 
-our $VERSION = '0.6010';
+our $VERSION = '0.6011';
 
 require Exporter;
 our @ISA    = qw(Exporter);
--- lib/Blender/Declare/Tutorial.pm
+++ lib/Blender/Declare/Tutorial.pm
@@ -6,23 +6,9 @@ Blender::Declare::Tutorial - Tutorial for use Blender-Declare.
 
 =head2 Installing
 
-    git clone https://github.com/magnolia-k/Blender-Declare.git
-    cd Blender-Declare
-    ./setup
-
-You can install Blender-Declare from Github.
-
-=head2 Write DSL
-
-=head2 Execute DSL
-
-=head2 Re-execute DSL
-
-=head1 Blender-Declare's DSL grammer.
-
-=head1 blender command
-
-=head1 FAQ
+    $ git clone https://github.com/magnolia-k/Blender-Declare.git
+    $ cd Blender-Declare
+    $ ./setup
 
 =cut
 
