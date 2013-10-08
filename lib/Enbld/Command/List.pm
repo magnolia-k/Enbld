ed1772ffce9ae5eafd6855c2c3832f86a4e7e22b
magnolia <magnolia.k@me.com>
2013-10-08 23:04:00 +0900

    'enbld list' can display specfic target's version list.

--- lib/Enbld/Command/List.pm
+++ lib/Enbld/Command/List.pm
@@ -13,6 +13,20 @@ require Enbld::App::Configuration;
 sub do {
     my $self = shift;
 
+    my $target_name = shift @{ $self->{argv} };
+
+	if ( $target_name ) {
+
+		$self->list_target_installed_version( $target_name );
+
+	} else {
+		$self->list_all_installed;
+	}
+}
+
+sub list_all_installed {
+	my $self = shift;
+
     Enbld::Home->initialize;
     Enbld::App::Configuration->read_file;
 
@@ -46,6 +60,29 @@ sub do {
     }
 
     return $self;
+}
+
+sub list_target_installed_version {
+	my $self = shift;
+	my $target_name = shift;
+
+    Enbld::Home->initialize;
+    Enbld::App::Configuration->read_file;
+
+    my $config = Enbld::App::Configuration->search_config( $target_name );
+
+    if ( ! $config ) {
+        require Enbld::Error;
+        die Enbld::Error->new( "Target '$target_name' is not installed yet.\n" );
+    }
+
+    for my $installed ( sort keys %{ $config->installed } ) {
+        print $installed . "\n";
+    }
+
+
+
+	return $self;
 }
 
 1;
