7876a45eb14a48d161bf3b434c8267d96ee30757
magnolia <magnolia.k@me.com>
2013-12-01 16:14:44 +0900

    Refactor definition 'wget'

--- lib/Enbld/Definition/Wget.pm
+++ lib/Enbld/Definition/Wget.pm
@@ -10,19 +10,16 @@ sub initialize {
     
     $self->SUPER::initialize;
     
-    $self->{defined}{ArchiveName}       =   'wget';
     $self->{defined}{WebSite}           =   'http://www.gnu.org/software/wget/';
-    $self->{defined}{VersionForm}       =   '\d{1,2}\.\d{1,2}(\.\d{1,2})?';
-    $self->{defined}{Extension}         =   'tar.gz';
+
     $self->{defined}{Dependencies}      =   [ 'libidn' ];
+
     $self->{defined}{DownloadSite}      =   'http://ftp.gnu.org/gnu/wget/';
+
+    $self->{defined}{ArchiveName}       =   'wget';
+    $self->{defined}{VersionForm}       =   '\d{1,2}\.\d{1,2}(\.\d{1,2})?';
     
     $self->{defined}{AdditionalArgument}=   \&set_argument;
-    
-    $self->{defined}{CommandConfigure}  =   './configure';
-    $self->{defined}{CommandMake}       =   'make';
-    $self->{defined}{CommandTest}       =   'make check';
-    $self->{defined}{CommandInstall}    =   'make install';
 
     return $self;
 }
