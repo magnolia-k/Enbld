c42ed3330be0e22f045b1996d561ead8e95e0ee9
magnolia <magnolia.k@me.com>
2013-12-01 16:34:37 +0900

    Refactor definition 'ruby'

--- lib/Enbld/Definition/Ruby.pm
+++ lib/Enbld/Definition/Ruby.pm
@@ -15,17 +15,11 @@ sub initialize {
     $self->{defined}{ArchiveName}    = 'ruby';
     $self->{defined}{WebSite}        = 'https://www.ruby-lang.org';
     $self->{defined}{VersionForm}    = '\d\.\d\.\d-p\d{1,3}';
-    $self->{defined}{Extension}      = 'tar.gz';
     $self->{defined}{Dependencies}   = \&set_Dependencies;
     $self->{defined}{DownloadSite}   = 'http://cache.ruby-lang.org/pub/ruby/';
 
     $self->{defined}{AdditionalArgument} = \&set_argument;
     $self->{defined}{SortedVersionList}  = \&set_sorted_version_list;
-
-    $self->{defined}{CommandConfigure}  = './configure';
-    $self->{defined}{CommandMake}       = 'make';
-    $self->{defined}{CommandTest}       = 'make test';
-    $self->{defined}{CommandInstall}    = 'make install';
 
     return $self;
 }
