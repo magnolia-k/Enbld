1bd4d4d565e6344c80081974edec6f9fa27d6427
magnolia <magnolia.k@me.com>
2013-12-01 17:04:27 +0900

    Refactor definition 'pkgconfig'

--- lib/Enbld/Definition/Pkgconfig.pm
+++ lib/Enbld/Definition/Pkgconfig.pm
@@ -14,14 +14,10 @@ sub initialize {
     $self->{defined}{WebSite}           =
         'http://www.freedesktop.org/wiki/Software/pkg-config/';
     $self->{defined}{VersionForm}       =   '\d\.\d{1,2}(?:\.\d)?';
-    $self->{defined}{Extension}         =   'tar.gz';
     $self->{defined}{DownloadSite}      =
         'http://pkgconfig.freedesktop.org/releases/';
 
-    $self->{defined}{CommandConfigure}  =   './configure --with-internal-glib';
-    $self->{defined}{CommandMake}       =   'make';
-    $self->{defined}{CommandTest}       =   'make check';
-    $self->{defined}{CommandInstall}    =   'make install';
+    $self->{defined}{AdditionalArgument} = '--with-internal-glib';
 
     return $self;
 }
