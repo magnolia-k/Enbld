872c7a7e170f74ce6da6f1f014ecef5d5493c71c
magnolia <magnolia.k@me.com>
2013-12-27 23:53:54 +0900

    Fix choose make commnad logic.

--- lib/Enbld/Target/AttributeExtension/Command.pm
+++ lib/Enbld/Target/AttributeExtension/Command.pm
@@ -6,13 +6,15 @@ use warnings;
 use parent qw/Enbld::Target::Attribute/;
 
 our $make;
+
 sub make_command {
+
     return $make if $make;
 
-    if ( `make -v` =~ /GNU Make/ ) {
-        return ( $make = 'make' );
-    } elsif ( `which gmake` ) {
+    if ( `which gmake` ) {
         return ( $make = 'gmake' );
+    } elsif ( `make -v 2>&1` =~ /GNU Make/ ) {
+        return ( $make = 'make' );
     } else {
         require Enbld::Error;
         die( Enbld::Error->new( 'GNU Make is NOT installed' ));
