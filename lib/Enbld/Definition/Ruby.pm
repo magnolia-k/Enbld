dcf7a60235fb81008c70269599b6f70fc33f3e64
magnolia <magnolia.k@me.com>
2014-02-26 23:36:57 +0900

    Improved definition for ruby.

--- lib/Enbld/Definition/Ruby.pm
+++ lib/Enbld/Definition/Ruby.pm
@@ -16,8 +16,11 @@ sub initialize {
     $self->{defined}{WebSite}        = 'https://www.ruby-lang.org';
     $self->{defined}{VersionForm}    = '\d\.\d\.\d(-p\d{1,3})?';
     $self->{defined}{Dependencies}   = [ 'openssl' ];
-    $self->{defined}{DownloadSite}   = 'http://cache.ruby-lang.org/pub/ruby/';
+    $self->{defined}{DownloadSite}   = 'https://ftp.ruby-lang.org/pub/ruby/';
 
+    $self->{defined}{VersionList}       =   \&set_versionlist;
+    $self->{defined}{URL}               =   \&set_url;
+ 
     $self->{defined}{AdditionalArgument} = \&set_argument;
     $self->{defined}{SortedVersionList}  = \&set_sorted_version_list;
 
@@ -34,6 +37,49 @@ sub set_sorted_version_list {
 
 sub set_argument {
     return "--with-openssl-dir=" . Enbld::Home->library;
+}
+
+sub set_versionlist {
+    my $attributes = shift;
+
+    require Enbld::HTTP;
+    my $first_html = Enbld::HTTP->get_html( $attributes->IndexSite );
+
+    my $first_list = $first_html->parse_version(
+            quotemeta( '<a href="') . '\d\.\d/' . quotemeta( '">' ),
+            '\d\.\d'
+            );
+
+    my @versionlist;
+    for my $ver ( @{ $first_list } ) {
+        my $html = Enbld::HTTP->get_html( $attributes->IndexSite . $ver );
+        my $list = $html->parse_version(
+                $attributes->IndexParserForm,
+                $attributes->VersionForm,
+                );
+
+        for my $version ( @{ $list } ) {
+            push @versionlist, $version;
+        }
+    }
+
+    return \@versionlist;
+}
+
+sub set_url {
+    my $attributes = shift;
+
+    my $ver = $attributes->Version;
+
+    my $major;
+    if ( $ver =~ /^(\d\.\d)\.\d.*$/ ) {
+        $major = $1;
+    }
+
+    my $filename = $attributes->Filename;
+    my $url = $attributes->DownloadSite . $major . '/' . $filename;
+
+    return $url;
 }
 
 1;
